8f1cbdd8d1c3cf9b4710e95a4fdeda3779c735a5
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-04-05 21:40:15 -0700

    submit1

--- warehouseapp.py
+++ warehouseapp.py
@@ -25,8 +25,8 @@ def readin():
         spamreader = csv.reader(csvfile)
         for row in spamreader:
             pro_id = int(row[0])
-            pro_x = int(round(float(row[1])) * 2) #double the cordinates to simulate the path
-            pro_y = int(round(float(row[2])) * 2) #then the robot only operate on coordinate with odd number
+            pro_x = int(int(float(row[1])) * 2)+1 #double the cordinates to simulate the path and move the rack from(0,0)to (1,1)
+            pro_y = int(int(float(row[2])) * 2)+1 #then the robot only operate on coordinate with even number
 
             loc_dict[pro_id] = [pro_x, pro_y]
     print "total goods num:", len(loc_dict)
@@ -39,26 +39,41 @@ def findpath(pro_id,init_x = 0, init_y = 0):
         print "id not exist"
         return -1
 
-    pro_x = loc_dict[pro_id][0]     #x,y coordinates
+    pro_x = loc_dict[pro_id][0]     #x,y coordinates of products
     pro_y = loc_dict[pro_id][1]
-    print "Searching product #", pro_id, " starting from position ", (init_x,init_y), "to product position", (pro_x,pro_y)
+    print "Searching product #", pro_id, "product position", (pro_x,pro_y)
     print "Calculating shortest path ......"
     #Product con only be taken from left or right, say east and west
     #if the product position is east to the current position i.e. init_x < prod_x
     if init_x < pro_x:
         des_x = pro_x - 1 #shorter to take from left path of the rack
         des_y = pro_y
-        if pro_x == 0:
-            des_x = pro_x + 1 #can only take from right since its the boundary, say wall
-            des_y = pro_y
+        # if pro_x == 0:
+        #     des_x = pro_x + 1 #can only take from right since its the boundary, say wall
+        #     des_y = pro_y
     #if the product position is west to the current position i.e. init_x > prod_x
-    if init_x > pro_x:
+    elif init_x > pro_x:
         des_x = pro_x + 1 #shorter to take from right path of the rack
         des_y = pro_y
-        if pro_x == 20:
-            des_x = pro_x - 1 #can only take from left since its the boundary, say wall
-            des_y = pro_y
-    distance = abs(des_x-init_x)+abs(des_y -init_y)
+        # if pro_x == 20:
+        #     des_x = pro_x - 1 #can only take from left since its the boundary, say wall
+        #     des_y = pro_y
+    else:
+        des_x = init_x
+        des_y = pro_y
+    print "desired position",  (des_x,des_y)
+
+    if init_x==0 and init_y ==0 or init_y % 2 == 0:
+        print "first x dir than y dir:", (init_x,init_y),"to",(des_x,init_y),"to",(des_x,des_y)
+        distance = abs(des_x - init_x) + abs(des_y - init_y)
+    else:
+        # rack in the way, 1 move in y direction first, update initial y
+        print"rack in the way",(init_x,init_y),"to", (init_x,init_y+1)
+        init_y = init_y+1
+        print "first x dir than y dir:", (init_x,init_y),"to",(des_x,init_y),"to",(des_x,des_y)
+        distance = abs(des_x - init_x) + abs(des_y - init_y)+1
+
+
     print "Total distance is : ", distance
 
 
